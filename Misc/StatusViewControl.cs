a11c1f03665962a6ab2309622848c509b6a75966
Wolfhardt <wolfhardt.freinbichler@maylab.net>
2017-05-31 17:07:41 +0200

    first

---
+++ Misc/StatusViewControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Forms;
+using MockPlugin.Device;
+
+namespace MockPlugin.Misc
+{
+    public partial class StatusViewControl : UserControl, AxelSemrau.Chronos.Plugin.IStatusView<MockDevice>
+    {
+        private MockDevice mDev;
+
+        public StatusViewControl()
+        {
+            InitializeComponent();
+        }
+
+        public MockDevice Device
+        {
+            get
+            {
+                return mDev;
+            }
+            set
+            {
+                mDev = value;
+                if (value != null)
+                {
+                    SimpleLabel.Text += " " + value.Name;
+                }
+            }
+        }
+    }
+}
---
+++ RemoteAccessTester/RemoteInterface.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceModel;
+
+namespace MockPlugin.RemoteAccessTester
+{
+    /// <summary>
+    /// Very basic service, only to show that you can trigger some action from outside and
+    /// get a response.
+    /// </summary>
+    [ServiceContract]
+    public interface IMockPlugin
+    {
+        [OperationContract]
+        bool DoSomething(string someParameter);
+    }
+    /// <summary>
+    /// To avoid duplicating the endpoint definition in server and client
+    /// </summary>
+    public static class EndpointDef
+    {
+        public const string Endpoint = "net.tcp://localhost:12543/RemotePluginService";
+    }
+}
